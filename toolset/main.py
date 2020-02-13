c6703148719d9cbd371a93c52aeae910821b226c
Leo Basov <l.basov@sparc-industries.com>
2020-02-13 12:45:53 +0100

    Midified main.

--- toolset/main.py
+++ toolset/main.py
@@ -5,12 +5,9 @@ import xml.etree.ElementTree as ET
 
 from mesh import*
 
-def convert_mesh_file(file_name):
-    mesh = Mesh()
-    tree = ET.parse(file_name)
-    root = tree.getroot()
-
 if __name__ == '__main__':
+    mesh = Mesh()
+
     parser = argparse.ArgumentParser(prog="Mesh data interpolation script", description='Reads mesh and control file. Interpolates control defined in cells to mesh nodes.')
     parser.add_argument('mesh', type=str, help='path to the mesh file')
     parser.add_argument('control', type=str, help='path to the control file')
@@ -18,7 +15,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     try:
-        convert_mesh_file(args.mesh)
+        mesh.read_from_xml(args.mesh)
     except Exception as e:
         print(e)
         exit()
