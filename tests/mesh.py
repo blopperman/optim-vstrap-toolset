be7ec323e610ddc55fb7d357b7141a1556a1db31
Leo Basov <l.basov@sparc-industries.com>
2020-02-13 10:45:48 +0100

    extended mesh.

--- tests/mesh.py
+++ tests/mesh.py
@@ -8,4 +8,11 @@ from toolset.mesh import Mesh
 class MeshTest(unittest.TestCase):
     def test_read_from_xml(self):
         mesh = Mesh()
-        mesh.read_from_xml('test_data/box_vol_regular_refined.xml')
+        execption = False
+
+        try:
+            mesh.read_from_xml('test_data/box_vol_regular_refined.xml')
+        except:
+            execption = True
+
+        self.assertTrue(not execption)
--- toolset/mesh.py
+++ toolset/mesh.py
@@ -3,14 +3,14 @@ import xml.etree.ElementTree as ET
 class Node:
     def __init__(self):
         self.id = 0
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
+        self.x_coord = 0.0
+        self.y_coord = 0.0
+        self.z_coord = 0.0
         self.value = (0, 0, 0)
 
 class Cell:
-    def __init__(self, id, nodes = []):
-        self.id = id
+    def __init__(self):
+        self.id = 0
         self.nodes_ids = []
         self.value = (0, 0, 0)
         self.volume = 0.0
@@ -18,6 +18,11 @@ class Cell:
     def set_nodes(self, nodes):
         for node in nodes:
             self.nodes_ids.append(node.id)
+
+        self.__calc_volume(nodes)
+
+    def __calc_volume(self, nodes):
+        pass
 
 class Mesh:
     def __init__(self):
@@ -36,6 +41,51 @@ class Mesh:
         self.nodes = {}
 
     def read_from_xml(self, file_name):
-        self.clear()
         tree = ET.parse(file_name)
         root = tree.getroot()
+
+        self.clear()
+        self.__check_mesh_file(root)
+        self.__create_nodes(root)
+        self.__create_cells(root)
+
+    def __check_mesh_file(self, root):
+        mesh_node = root.find('mesh')
+        mesh_type = mesh_node.get('mesh_type')
+
+        if mesh_type != "volume_mesh":
+            raise Exception(__name__, self.__check_mesh_file.__name__, 'volume_mesh not defined in file')
+
+    def __create_nodes(self, root):
+        for element in root.find('nodes').findall('node'):
+            node = Node()
+
+            node.id = int(element.get('node_number'))
+            node.x_coord = float(element.get('x_coord'))
+            node.y_coord = float(element.get('y_coord'))
+            node.z_coord = float(element.get('z_coord'))
+
+            if node.id not in self.nodes:
+                self.nodes[node.id] = node
+            else:
+                raise Exception(__name__, self.__create_nodes.__name__, 'Node with id {} allready in list.'.format(node.id))
+
+    def __create_cells(self, root):
+        for element in root.find('elements').findall('element'):
+            cell = Cell()
+            node_ids = self.__get_node_ids(element.find('nodes').text)
+            nodes = []
+
+            for id in node_ids:
+                nodes.append(self.nodes[id])
+
+            cell.id = int(element.get('elem_number'))
+            cell.set_nodes(nodes)
+
+    def __get_node_ids(self, text):
+        node_ids = []
+
+        for str in text.split(','):
+            node_ids.append(int(str))
+
+        return node_ids
